bb20117cf1e1dc35546cd6b799da7e31d485d532
rbacollado <33411046+rbacollado@users.noreply.github.com>
2020-09-23 22:33:54 +0800

    fixed axios url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-5a57e/us-central1/api", // api URL
+  baseURL: "https://us-central1-clone-5a57e.cloudfunctions.net/api", // api URL
 });
 
 export default instance;
+//localhost: http://localhost:5001/clone-5a57e/us-central1/api --debug locally
